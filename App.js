ceaadbf7047d95297f039a0dfee593ef421bd717
= <=>
2017-09-11 21:08:15 +0800

    Initial Commit

---
+++ App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+import { Provider } from 'react-redux'
+
+import configureStore from './src/store/configureStore'
+import App from './src/containers/App'
+
+const store = configureStore()
+
+export default class extends Component {
+  render() {
+    return (
+      <Provider store={store}>
+        <App />
+      </Provider>
+    )
+  }
+}
